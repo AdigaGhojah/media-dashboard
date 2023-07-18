682da68ac103c9bacbbaf25d45ae04b841cd68f9
Ahmad <ahmadghojah1996@gmail.com>
2023-07-18 20:58:19 +0300

    fix add/edit group/company page

--- pages/groups-list/[id].vue
+++ pages/groups-list/[id].vue
@@ -2,39 +2,48 @@
   <q-layout>
     <q-page-container>
       <q-page>
-        <div class="q-pa-md" style="max-width: 400px;width:100%">
-        <h4 class="text-h4 q-mt-none">{{pageTitle}}</h4>
+        <div class="q-pa-md" style="max-width: 400px; width: 100%">
+          <h4 class="text-h4 q-mt-none">{{ pageTitle }}</h4>
           <q-form @submit="onSubmit" class="q-gutter-sm">
             <q-input
-            outlined 
+              outlined
               v-model="name"
-              label="Name *"
+              label="Company Name *"
               lazy-rules
               :rules="[
                 (val) => (val && val.length > 0) || 'This field is required',
               ]"
             />
             <q-input
-            outlined 
-              v-model="email"
-              label="Email *"
+              outlined
+              v-model="phoneNumber"
+              type="tel"
+              label="Phone Number *"
               lazy-rules
               :rules="[
                 (val) => (val && val.length > 0) || 'This field is required',
-                (val) => val.toLowerCase().match(emailRegex) || 'Invalid email',
               ]"
             />
 
             <q-input
-            outlined 
-              type="password"
-              v-model="password"
-              label="Password *"
+              outlined
+              v-model="address"
+              label="Address *"
               lazy-rules
               :rules="[
                 (val) =>
                   (val !== null && val !== '') || 'This field is required',
-                  (val) => (val < 6) || 'The password length should be more than 6 character',
+              ]"
+            />
+
+            <q-input
+              outlined
+              v-model="refCode"
+              label="Ref Code *"
+              lazy-rules
+              :rules="[
+                (val) =>
+                  (val !== null && val !== '') || 'This field is required',
               ]"
             />
 
@@ -48,23 +57,20 @@
   </q-layout>
 </template>
 <script setup>
-
 const route = useRoute();
-const pageTitle = ref(`Edit ${route.params.id} Profile`);
+const pageTitle = ref(`Edit ${route.params.id} Company`);
 const submitBtnLabel = ref(`update`);
-  if(route.params.id=='create')
-  {
-    pageTitle.value='Add Employee';
-    submitBtnLabel.value= 'Create'
-  }
-const emailRegex = ref(
-  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
-);
+if (route.params.id == "create") {
+  pageTitle.value = "Add Company";
+  submitBtnLabel.value = "Create";
+}
 const name = ref(null);
-const email = ref(null);
-const password = ref(null);
+const phoneNumber = ref(null);
+const address = ref(null);
+const refCode = ref(null);
 
 const onSubmit = async () => {
-    console.log(3212);
+  console.log(3212);
 };
 </script>
+
